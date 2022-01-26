f8930c63dbe14d729344d7e08048e6857911e436
KyleHu14 <lloydpearce4@gmail.com>
2022-01-26 00:24:36 -0800

    frontend can add question via api

--- client/src/components/InputQuestion.js
+++ client/src/components/InputQuestion.js
@@ -5,23 +5,48 @@ const InputQuestion = () => {
     const [questionAnswer, setQuestionAnswer] = useState("");
 
     const onSubmitForm = async (e) => {
+        // Prevents a submit button from submitting a form
         e.preventDefault();
+        try {
+            const body = { questionBody, questionAnswer };
+            const res = await fetch("http://localhost:5000/quiz", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(body),
+            });
+
+            // Reset the form
+            setQuestionAnswer("");
+            setQuestionBody("");
+        } catch (error) {
+            console.log(error.message);
+        }
     };
 
     return (
         <Fragment>
-            <h2>Input Todo</h2>
-            <form className="form-control">
+            <h2>Create a Question!</h2>
+            <form className="form-control" onSubmit={onSubmitForm}>
                 <div className="input-group">
-                    <label>Label Field</label>
-                    <input type="text" />
+                    <label>Question</label>
+                    <input
+                        type="text"
+                        placeholder="What is your question?"
+                        value={questionBody}
+                        onChange={(e) => setQuestionBody(e.target.value)}
+                    />
                 </div>
                 <div className="input-group">
-                    <label>Label Field</label>
-                    <input type="text" />
+                    <label>Answer</label>
+                    <input
+                        type="text"
+                        placeholder="What is your answer?"
+                        value={questionAnswer}
+                        onChange={(e) => setQuestionAnswer(e.target.value)}
+                    />
                 </div>
-                <button className="submit-control" type="submit">
-                    SUBMIT
+                <button className="btn" type="submit">
+                    Add Question
                 </button>
             </form>
         </Fragment>
